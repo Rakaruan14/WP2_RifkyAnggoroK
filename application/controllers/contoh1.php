e1fd2bf934db36b64c0d9d1e364f261cf76dc436
Rakaruan14 <anggarifky41@gmail.com>
2022-09-30 15:38:05 +0700

    penjumlahan

--- application/controllers/contoh1.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Contoh1 extends CI_Controller
-{
-    public function index()
-    {
-        echo"<h1>Perkenalkan</h1>";
-        echo"Nama saya Anggoro Kusumo. Saya tinggal di daerah Sragen olahraga yang saya sukai adalah Bulu Tangssss";
-    }
-}
