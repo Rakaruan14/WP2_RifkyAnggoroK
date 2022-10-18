f53a861fae20be4bb2704ad127fd751d0978e16b
Rakaruan14 <anggarifky41@gmail.com>
2022-10-18 21:53:33 +0700

    ganti nama

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -1,18 +1,21 @@
 <?php
-defined('BASEPATH') or exit ('no direct script access allowed');
+defined('BASEPATH') or exit('no direct script access allowed');
 
-class web extends CI_Controller{
+class web extends CI_Controller
+{
 
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
         $this->load->helper('url');
     }
 
-    public function index(){
+    public function index()
+    {
         $data['judul'] = 'Halaman Depan';
-        $this->load->view('v_header',$data);
-        $this->load->view('v_index',$data);
-        $this->load->view('v_footer',$data);
+        $this->load->view('v_header', $data);
+        $this->load->view('v_index', $data);
+        $this->load->view('v_footer', $data);
     }
 
     public function about()
